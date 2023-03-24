ff768942bb235ee1391b078f89e309cd2d1f72cd
liam cottrell <liamcottrell1996@hotmail.co.uk>
2023-03-24 14:47:41 +0000

    chnaging script 1 and 2

--- python-survey-app/script2.py
+++ python-survey-app/script2.py
@@ -1,3 +1,18 @@
-from csvhelper import read_csv
+# from csvhelper import read_csv
+import csv
+from script1 import ticket_6
 
-print(read_csv('clean_results.csv'))
+# print(read_csv('clean_results.csv'))
+for line in ticket_6:
+    print(line)
+
+
+print(read_csv())
+
+# def read_csv(filename):
+#     rows = []
+#     with open(filename, 'r') as f:
+#         reader = csv.reader(f, delimiter=',')
+#         for row in reader:
+#             rows.append(row)
+#     return rows
