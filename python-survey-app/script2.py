ba03a661f9bff96d65a8677a78a3138f68eab75e
Liam Cottrell <liamcottrell1996@hotmail.co.uk>
2023-03-27 16:45:35 +0100

    Update script2.py

--- python-survey-app/script2.py
+++ python-survey-app/script2.py
@@ -1,15 +1,20 @@
-# from csvhelper import read_csv
-import csv
-from script1 import ticket_6
+# # from csvhelper import read_csv
+# import csv
+# from script1 import ticket_6
 
-# print(read_csv('clean_results.csv'))
-import csv
+# # print(read_csv('clean_results.csv'))
+# import csv
 
-with open("clean_results.csv") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(" ".join(row))
+# with open("clean_results.csv") as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print(" ".join(row))
 
+
+
+print("hi")
+        
+        
 
 
 # def read_csv(filename):
